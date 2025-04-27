dc566036b14279bbf05cca84e756074d9b4419a9
rajeswari-Machina <rajeswarimachina02@gmail.com>
2025-04-27 21:23:04 +0530

    modidfied main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import os
 from app.api.routes import router
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
@@ -8,9 +9,14 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["http://localhost:5173","https://askme-ai-1.onrender.com"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000)) 
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
