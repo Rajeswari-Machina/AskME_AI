1a642c8052d30d5b10884a7dbb23738279aafcd3
rajeswari-Machina <rajeswarimachina02@gmail.com>
2025-04-28 06:53:57 +0530

    cors policy fixes

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://askme-ai-1.onrender.com"], 
+    allow_origins=["*"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
--- backend/app/services/qa_service.py
+++ backend/app/services/qa_service.py
@@ -2,16 +2,46 @@ from app.models.vector_store import get_vector_store
 from transformers import pipeline
 from langchain.llms import HuggingFacePipeline
 from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
 
-qa_pipeline = pipeline("text2text-generation", model="google/flan-t5-base", device=-1)
+
+prompt_template = PromptTemplate(
+    input_variables=["question"],
+    template="""
+You are a knowledgeable assistant. When answering, explain concepts clearly and in a detailed, step-by-step manner.
+Answer this question:
+
+Context: {context}
+
+Question: {question}
+
+Answer in long, clear, detailed sentences:
+"""
+)
+
+
+qa_pipeline = pipeline(
+    "text2text-generation",
+    model="google/flan-t5-base",
+    device=-1,
+    max_length=1024,       
+    temperature=0.7        
+)
 llm = HuggingFacePipeline(pipeline=qa_pipeline)
+
 
 def get_answer(question: str) -> str:
     vector_store = get_vector_store()
     if vector_store is None:
         raise ValueError("No PDF uploaded yet.")
 
-    qa = RetrievalQA.from_chain_type(llm=llm, retriever=vector_store.as_retriever())
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        chain_type_kwargs={"prompt": prompt_template}
+    )
+    
     response = qa(question)
     print(response)
+
     return response
